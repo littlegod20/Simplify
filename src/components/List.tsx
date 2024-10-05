e8b49a38e2b8f26335acb634eb0c2545fdca7362
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-10-05 11:36:46 +0000

    added the update functionality

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -12,6 +12,33 @@ const List: React.FC<ListProps> = ({ todos, setTodos }) => {
     );
   };
 
+  const onUpdateTodo = (
+    a: Partial<TodoProps>,
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    const updateTodo = todos.find((item) => item.id === a.id);
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo === updateTodo ? { ...updateTodo, [name]: value } : todo
+      )
+    );
+  };
+
+  // const onUpdateTodoSelect = (
+  //   a: Partial<TodoProps>,
+  //   e: React.ChangeEvent<HTMLSelectElement>
+  // ) => {
+  //   const updateTodo = todos.find((item) => item.id === a.id);
+  //   setTodos((prevTodos) =>
+  //     prevTodos.map((todo) =>
+  //       todo === updateTodo ? { ...updateTodo, routine: e.target.value } : todo
+  //     )
+  //   );
+  // };
+
   return (
     <>
       <section className="pt-6 w-full ">
@@ -22,21 +49,38 @@ const List: React.FC<ListProps> = ({ todos, setTodos }) => {
           >
             {item.isEditting ? (
               <input
-                className="focus:outline-none focus-visible:ring-1 focus-visible:ring-black border rounded-md p-2"
-                // value={item.text}
+                name="text"
+                className="focus:outline-none focus-visible:ring-1 focus-visible:ring-black border rounded-md p-2 w-44"
+                value={item.text}
+                onChange={(e) => onUpdateTodo(item, e)}
               />
             ) : (
               <div className="flex flex-row gap-2">
                 <input type="checkbox" />
-                <p> {item.text} </p>
+                <p className="max-w-44 truncate"> {item.text} </p>
               </div>
             )}
 
             <div className="flex flex-row justify-center items-center space-x-2">
-              <p className=" flex justify-center items-center hover:bg-slate-100 p-2 text-xs border rounded-md ">
-                {/* add a functionality that when the checkbox is ticked the color of the todo changes to green */}
-                {item.routine}
-              </p>
+              {item.isEditting ? (
+                <p>
+                  <select
+                    name="routine"
+                    className="p-2 rounded-md"
+                    value={item.routine}
+                    onChange={(e) => onUpdateTodo(item, e)}
+                  >
+                    <option value="personal">Personal</option>
+                    <option value="work">Work</option>
+                    <option value="shopping">Shopping</option>
+                  </select>
+                </p>
+              ) : (
+                <p className=" flex justify-center items-center hover:bg-slate-100 p-2 text-xs border rounded-md ">
+                  {/* add a functionality that when the checkbox is ticked the color of the todo changes to green */}
+                  {item.routine}
+                </p>
+              )}
               <div
                 className="rounded-md  hover:bg-slate-100 transition-all duration-300 ease-in p-3"
                 onClick={() => {
